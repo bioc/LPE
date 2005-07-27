a4631ac171ef85e96312054fee0e5f0e639d9c0c
Nitin Jain <nitin.jain@pfizer.com>
2005-07-27 16:09:03 +0000

    Updated baseOlig.error.step1.R to take care of NA values in some intervals
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/LPE@13380 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/baseOlig.error.step1.R
+++ R/baseOlig.error.step1.R
@@ -13,27 +13,65 @@ baseOlig.error.step1 <- function(y, stats=median, q=0.01, df=10) {
   medianAs <- rep(NA, length = quan.n)
   
   # If minimum expression value(s) is more than q percentage of total
-  # data, then exclude those minimum numbers for
-  # calculating quantiles
+  # data, then exclude those minimum numbers for calculating quantiles
 
-  if(sum(A == min(A)) > (q * length(A))) {
-    tmpA <- A[!(A == min(A))]
-    quantile.A <- c(min(A), quantile(tmpA, probs = seq(q, 1, q), na.rm = TRUE))
-  }
+  if(sum(A == min(A)) > (q * length(A)))
+    {
+      tmpA <- A[!(A == min(A))]
 
-  for(i in 2:(quan.n + 1)) {
-    n.i <- length(!is.na(M[A > quantile.A[i - 1] & A <= quantile.A[i]]))
-    mult.factor <- 0.5*((n.i - 0.5)/(n.i -1))
-    var.M[i - 1] <- mult.factor * var(M[A > quantile.A[i - 1] & A <= quantile.A[i]], na.rm=TRUE) 
-    medianAs[i - 1] <- median(A[A > quantile.A[i - 1] & A <= quantile.A[i]], na.rm = TRUE)
-  }
-  var.M[1:which(var.M==max(var.M))] <- max(var.M)
+      quantile.A <- c(min(A), quantile(tmpA, probs = seq(q, 1, q),
+                                       na.rm = TRUE))
+    }
+
+  for (i in 2:(quan.n + 1))
+    {
+      n.i <- length(!is.na(M[A > quantile.A[i - 1] & A <= quantile.A[i]]))
+      
+      if (n.i >1)
+        {
+          mult.factor <- 0.5*((n.i - 0.5)/(n.i -1))
+          
+          var.M[i - 1] <- mult.factor * var(M[A > quantile.A[i - 1] & A <=
+                                              quantile.A[i]], na.rm=TRUE)
+          
+          medianAs[i - 1] <- median(A[A > quantile.A[i - 1] & A <=
+                                      quantile.A[i]], na.rm = TRUE)
+        }
+      
+    } ## End of "for (i in 2:...)" loop
   
+  
+  ## In some intervals, if there are many thresholded points, then a
+  ## few consecutive quantiles of A are same, causing n.i as zero!!
+  ## For such intervals, assign the interval variance to the average
+  ## variance of the previous and next interval if previous interval
+  ## has non-NA variance, else assign it to that of next
+  ## interval. Logic is that: at the high intensity region, the genes
+  ## are sparse, i.e. it is unlikely that the consecutive quantiles in
+  ## the max intensity region are same.
+  
+  if (any(is.na(var.M)) )
+    {
+      for (i in (quan.n-1):1 )
+        {
+          if (is.na(var.M[i]))
+            {
+              var.M[i] <- ifelse(!is.na(var.M[i-1]),
+                                 mean(var.M[i+1], var.M[i-1]),
+                                 var.M[i+1])
+            }
+        }
+    }
+
+  ## Correct the mathematical artifact of low intensity region
+  var.M[1:which(var.M==max(var.M))] <- max(var.M)
+
   base.var <- cbind(A = medianAs, var.M = var.M)
   sm.spline <- smooth.spline(base.var[, 1], base.var[, 2], df = df)
   min.Var <- min(base.var[,2])
   var.genes <- fixbounds.predict.smooth.spline(sm.spline, median.y)$y
-  var.genes[var.genes < min.Var] <- min.Var
+  if (any(var.genes <min.Var))
+    var.genes[var.genes < min.Var] <- min.Var
   basevar.step1 <- cbind(A = median.y, var.M = var.genes)
   
   ord.median <- order(basevar.step1[,1])
