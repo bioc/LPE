b5fbb53b7967c0a2b32b2b299d41d3f74d815476
Nitin Jain <nitin.jain@pfizer.com>
2005-07-27 16:47:03 +0000

    Updated am.trans to ensure maximum 5 reps are used
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/LPE@13388 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/am.trans.R
+++ R/am.trans.R
@@ -1,21 +1,22 @@
-am.trans <- function(y) {
-  n <- ncol(y)
-  if (n < 2) {
-    stop("There are no replicated arrays!")
-  } 
-  A <- c()
-  M <- c()
-  cc <- permute(1:n)
-  for (i in 1:(n-1)) {
-    A <- c(A, c((y + y[,cc[i,]])/2), recursive=TRUE)
-    M <- c(M, c(y - y[,cc[i,]]), recursive=TRUE)
+am.trans <- function (y)
+  {
+    if(ncol(y)>5)
+      y <- y[,sample(1:ncol(y),5)]
+    n <- ncol(y)
+
+    if (n < 2)
+      {
+        stop("There are no replicated arrays!")
+      }
+
+    A <- c()
+    M <- c()
+    cc <- permute(1:n)
+
+    for (i in 1:(n - 1))
+      {
+        A <- c(A, c((y + y[, cc[i, ]])/2), recursive = TRUE)
+        M <- c(M, c(y - y[, cc[i, ]]), recursive = TRUE)
+      }
+    return(cbind(A, M))
   }
-  return(cbind(A,M))
-}
-
-# The above function transforms the replicated arrays
-# in the (A,M) format and duplicates the data
-# eg: (Y1+Y2, Y1-Y2) and (Y2+Y1, Y2-Y1) are the 
-# four columns returned for input column Y1 and Y2
-# i.e. duplicate arrays
-
