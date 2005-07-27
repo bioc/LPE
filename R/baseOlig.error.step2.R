037763528ab2980d33011b7b55c3eeb87ecebc70
Nitin Jain <nitin.jain@pfizer.com>
2005-07-27 16:10:41 +0000

    Updated baseOlig.error.step2.R to take care of NA values in some intervals
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/LPE@13381 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/baseOlig.error.step2.R
+++ R/baseOlig.error.step2.R
@@ -21,18 +21,36 @@ genes.sub.int <- n.genes.adaptive.int(var.genes.ord,min.genes.int=min.genes.int,
 	vect.temp <- (A > var.genes.ord[j.start,1] & 
 			A <= var.genes.ord[j.end,1])
 	n.i <- length(!is.na(M[vect.temp]))
-        mult.factor <- 0.5 * ((n.i - 0.5)/(n.i - 1))
-        var.M.adap[i - 1] <- mult.factor * var(M[vect.temp], na.rm = TRUE)
-        medianAs.adap[i - 1] <- median(A[vect.temp], na.rm = TRUE)
-  }
+         if (n.i >1)
+           {
+             mult.factor <- 0.5 * ((n.i - 0.5)/(n.i - 1))
+             var.M.adap[i - 1] <- mult.factor * var(M[vect.temp], na.rm = TRUE)
+             medianAs.adap[i - 1] <- median(A[vect.temp], na.rm = TRUE)
+           }
+      }
 
+ if (any(is.na(var.M.adap)) )
+    {
+      for (i in length(genes.sub.int):1 )
+        {
+          if (is.na(var.M.adap[i]))
+            {
+              var.M.adap[i] <- ifelse(!is.na(var.M.adap[i-1]),
+                                 mean(var.M.adap[i+1], var.M.adap[i-1]),
+                                 var.M.adap[i+1])
+            }
+        }
+    }
+
+        
   var.M.adap[1:which(var.M.adap == max(var.M.adap))] <- max(var.M.adap)
 
   base.var.adap <- cbind(A.adap = medianAs.adap, var.M.adap = var.M.adap)
   sm.spline.adap <- smooth.spline(base.var.adap[, 1], base.var.adap[, 2], df = df)
   min.Var <- min(base.var.adap[,2])
   var.genes.adap <- fixbounds.predict.smooth.spline(sm.spline.adap, median.y)$y
-  var.genes.adap[var.genes.adap < min.Var] <- min.Var
+  if (any(var.genes.adap < min.Var))
+    var.genes.adap[var.genes.adap < min.Var] <- min.Var
   basevar.all.adap <- cbind(A = median.y, var.M = var.genes.adap)
   
  return(basevar.all.adap)
